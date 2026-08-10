b6599fc8041e7ff4ed66f9d49f57404ce7f180fa
Gloriya Topno <gloriyatopno8@gmail.com>
2026-08-10 19:08:18 +0530

    Practice Arithmetic Operation

---
+++ Practice.js
@@ -0,0 +1,8 @@
+console.log("Arithmetic Operations in Node.js");
+a=10;
+b=5;
+console.log("The sum of a and b is: " + (a + b));
+console.log("The difference of a and b is: " + (a - b));
+console.log("The product of a and b is: " + (a * b));
+console.log("The quotient of a and b is: " + (a / b));
+console.log("The remainder of a and b is: " + (a % b));
