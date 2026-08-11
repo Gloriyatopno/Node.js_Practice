67f5f44003c2d2d002b87b5094ebbf7f9efd2b8e
Gloriya Topno <gloriyatopno8@gmail.com>
2026-08-11 21:42:04 +0530

    Implement npm validator

--- Node_js_Modules_npm/app.js
+++ Node_js_Modules_npm/app.js
@@ -1,5 +1,8 @@
+const validate = require('validator');
 const Arithmetic = require('./Arithmetic.js');
 
 const sum = Arithmetic(5, 10);
 console.log(sum);
 
+console.log(validate.isEmail('gloriya@gmail.com'));
+console.log(validate.isURL('https://www.google.com'));
