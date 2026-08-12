d9ce405e170c80fb8c29bedc991b502e5cd47e1c
Gloriya Topno <gloriyatopno8@gmail.com>
2026-08-12 18:46:58 +0530

    Implement process.argv

--- Node_js_Modules_npm/app.js
+++ Node_js_Modules_npm/app.js
@@ -1,14 +1,21 @@
-const chalk = require('chalk');
-const validate = require('validator');
-const Arithmetic = require('./Arithmetic.js');
+const chalk = require('chalk')
+const validate = require('validator')
+const Arithmetic = require('./Arithmetic.js')
 
-const sum = Arithmetic(5, 10);
-console.log(sum);
+const sum = Arithmetic(5, 10)
+console.log(sum)
 
-console.log(validate.isEmail('gloriya@gmail.com'));
-console.log(validate.isURL('https://www.google.com'));
+console.log(validate.isEmail('gloriya@gmail.com'))
+console.log(validate.isURL('https://www.google.com'))
 
-const bluemsg = chalk.bold.inverse.blue('Hello, World!');
-console.log(bluemsg);
+const bluemsg = chalk.bold.inverse.blue('Hello, World!')
+console.log(bluemsg)
 
-console.log(chalk.red('Success!'));
+console.log(chalk.red('Success!'))
+
+const command = process.argv[2]
+if (command === 'add') {
+    console.log('Adding Note!')
+} else if (command === 'remove') {
+    console.log('Removing Note!')
+}
