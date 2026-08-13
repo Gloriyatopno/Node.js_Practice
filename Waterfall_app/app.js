d8c30b7b627ddffc4300cbd225b3b1f66f08b281
Gloriya Topno <gloriyatopno8@gmail.com>
2026-08-14 04:34:02 +0530

    Implement Error Handling

--- Waterfall_app/app.js
+++ Waterfall_app/app.js
@@ -12,20 +12,32 @@ console.log('Stopping')*/
 
 const request = require('request')
 
-const url = 'https://api.weatherstack.com/current?access_key=dc04b9d75396821c906d02c5a11bdbb9&query=37.8267,-122.4233&units=f'
+/*const url = 'https://api.weatherstack.com/current?access_key=dc04b9d75396821c906d02c5a11bdbb9&query=37.8267,-122.4233&units=f'
 
 request({ url: url, json: true }, (error, response) => {
    // console.log(response.body.current)
+   if (error) {
+       console.log('Unable to connect to weather service!')
+    } else if (response.body.error) {
+        console.log('Unable to find location')
+     }else {
     console.log(response.body.current.weather_descriptions[0] + ". It is currently " +response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees out.")
-})
+     } 
+})*/
 
 //Geocoding
 //Address -> Lat/Long -> Weather
 
 const geocodeURL = 'https://api.positionstack.com/v1/forward?access_key=6b90fd1518718a918c1314d95d179c2c&query=1600%20Pennsylvania%20Ave%20NW%2C%20Washington%20DC'
 request({ url: geocodeURL, json: true }, (error, response) => {
+
+    if (error) {
+        console.log('Unable to connect to location services!')
+    }else if (response.body.data.length === 0)  {
+        console.log('Unable to find location. Try another search.')
+    }else {
     const latitude = response.body.data[0].latitude
     const longitude = response.body.data[0].longitude
     console.log(latitude, longitude)
-
+    }
 })
